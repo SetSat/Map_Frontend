db555f09759e7989457799b54c0546cc7c9d2c45
Sathish <sathishsettu9298@gmail.com>
2024-09-04 21:00:10 +0530

    icon update

--- vechilemap/src/VehicleMap.jsx
+++ vechilemap/src/VehicleMap.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Polyline,
-  Popup,
-} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Polyline } from "react-leaflet";
+import L from "leaflet";
 
 import "leaflet/dist/leaflet.css";
+
+const defaultIcon = L.icon({
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
 
 const VehicleMap = () => {
   const [vehicleLocation, setVehicleLocation] = useState([
@@ -43,7 +46,7 @@ const VehicleMap = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
 
-      <Marker position={vehicleLocation} />
+      <Marker position={vehicleLocation} icon={defaultIcon} />
 
       <Polyline positions={routePath} color="blue" />
     </MapContainer>
