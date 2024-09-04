4c428b2a02b96d56e6829632d2d15a8994c68984
Sathish <sathishsettu9298@gmail.com>
2024-09-04 20:28:17 +0530

    commit 1

---
+++ vechilemap/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VehicleMap from "./VehicleMap";
+
+const App = () => {
+  return (
+    <div>
+      <VehicleMap></VehicleMap>
+    </div>
+  );
+};
+
+export default App;
---
+++ vechilemap/src/VehicleMap.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Polyline,
+  Popup,
+} from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+
+const VehicleMap = () => {
+  const [vehicleLocation, setVehicleLocation] = useState([
+    17.385044, 78.486671,
+  ]);
+  const [routePath, setRoutePath] = useState([[17.385044, 78.486671]]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("http://localhost:4000/api/vehicle-location")
+        .then((res) => res.json())
+        .then((data) => {
+          const newLocation = [data.latitude, data.longitude];
+          setVehicleLocation(newLocation);
+          setRoutePath((prevPath) => [...prevPath, newLocation]);
+        })
+        .catch((error) =>
+          console.error("Error fetching vehicle location:", error)
+        );
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <MapContainer
+      center={vehicleLocation}
+      zoom={13}
+      style={{ height: "900px", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+      <Marker position={vehicleLocation} />
+
+      <Polyline positions={routePath} color="blue" />
+    </MapContainer>
+  );
+};
+
+export default VehicleMap;
